338bdbbca7890e2d399f024b4a222074c257b9f5
Lystea Alt <lysdragon224@gmail.com>
2025-08-05 00:11:33 +0000

    The M logo is completely messed up now. Change it to a normal M with nic

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -1,16 +1,22 @@
 export function Logo(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg
-      xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
+      fill="currentColor"
+      xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <path d="M2 2l8 20 8-20L2 2z M12 2v20" />
+      <text
+        x="50%"
+        y="50%"
+        dominantBaseline="central"
+        textAnchor="middle"
+        fontSize="22"
+        fontWeight="bold"
+        fontFamily="sans-serif"
+      >
+        M
+      </text>
     </svg>
   );
 }
